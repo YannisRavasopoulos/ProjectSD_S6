5296414c13aea267607888c1f6b66b164439559f
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-05-01 19:56:25 +0300

    Cleanup

--- frontend/lib/data/service/authentication_service.dart
+++ frontend/lib/data/service/authentication_service.dart
@@ -1,17 +1,23 @@
 import 'dart:convert';
 
-import 'package:frontend/data/service/json_web_token.dart';
+import 'package:frontend/config.dart';
+import 'package:frontend/data/authentication_exception.dart';
+import 'package:frontend/data/json_web_token.dart';
 import 'package:http/http.dart' as http;
-
-const apiDomain = 'localhost';
 
 /// This class is responsible for handling authentication-related tasks.
 class AuthenticationService {
   static final loginUri = Uri.https(apiDomain, '/auth/login');
   static final logoutUri = Uri.https(apiDomain, '/auth/logout');
+  static final refreshUri = Uri.https(apiDomain, '/auth/refresh');
+
+  final http.Client client;
+
+  AuthenticationService({http.Client? client})
+    : client = client ?? http.Client();
 
   Future<JsonWebToken> login(String username, String password) async {
-    final response = await http.post(
+    final response = await client.post(
       loginUri,
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'email': username, 'password': password}),
@@ -24,22 +30,21 @@ class AuthenticationService {
     }
   }
 
+  Future<JsonWebToken> refresh(JsonWebToken token) async {
+    final response = await http.post(refreshUri, headers: token.makeHeaders());
+
+    if (response.statusCode == 200) {
+      return JsonWebToken.fromJson(jsonDecode(response.body));
+    } else {
+      throw AuthenticationException('Failed to refresh token');
+    }
+  }
+
   Future<void> logout(JsonWebToken token) async {
     final response = await http.post(logoutUri, headers: token.makeHeaders());
 
     if (response.statusCode != 200) {
       throw AuthenticationException('Failed to logout');
     }
-  }
-}
-
-class AuthenticationException implements Exception {
-  final String message;
-
-  AuthenticationException(this.message);
-
-  @override
-  String toString() {
-    return 'AuthenticationException: $message';
   }
 }
--- frontend/lib/data/service/user_service.dart
+++ frontend/lib/data/service/user_service.dart
@@ -1,3 +1,3 @@
 class UserService {
-  void createUser(String username, String password) {}
+  void getUser() {}
 }
