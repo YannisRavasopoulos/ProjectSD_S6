c53523e94803eaba691167d78921a8b7632677d1
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-05-03 20:55:36 +0300

    Separate schemas

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,22 +1,5 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from fastapi import Depends
-from services.auth_service import SignInSchema, signin
-
-from pydantic import BaseModel
-
-# Define schemas first
-# class SignUpSchema(BaseModel):
-    # name: str
-    # email: str
-    # password: str
-    # role: str  # Default to carpooler if not specified
-
-class LoginSchema(BaseModel):
-    email: str
-    password: str
-
-
 
 router = APIRouter()
 
