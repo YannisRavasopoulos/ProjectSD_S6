7843866431464f7b7582206454aa97e316b4d723
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-05-23 20:32:39 +0300

    Disable non working repos

--- frontend/lib/data/repository/authentication_repository.dart
+++ frontend/lib/data/repository/authentication_repository.dart
@@ -1,20 +1,20 @@
-import 'package:frontend/data/authentication.dart';
-import 'package:frontend/data/json_web_token.dart';
+// import 'package:frontend/data/authentication.dart';
+// import 'package:frontend/data/json_web_token.dart';
 
-// TODO
-class AuthenticationRepository {
-  Authentication? _cached;
+// // TODO
+// class AuthenticationRepository {
+//   Authentication? _cached;
 
-  Future<void> addAuthentication(String email, String password) async {
-    _cached = JsonWebToken.fake(1);
-    await Future.delayed(Duration(seconds: 1));
-  }
+//   Future<void> addAuthentication(String email, String password) async {
+//     _cached = JsonWebToken.fake(1);
+//     await Future.delayed(Duration(seconds: 1));
+//   }
 
-  Future<Authentication?> getAuthentication() async {
-    return _cached;
-  }
+//   Future<Authentication?> getAuthentication() async {
+//     return _cached;
+//   }
 
-  Future<void> clearAuthentication() async {
-    throw UnimplementedError();
-  }
-}
+//   Future<void> clearAuthentication() async {
+//     throw UnimplementedError();
+//   }
+// }
