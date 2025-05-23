298c3706aa2fefd0000550f353ea3bb49ac22a09
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-05-24 00:42:50 +0300

    Update app.dart

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -7,60 +7,42 @@ import 'package:frontend/data/repository/location_repository.dart';
 import 'package:frontend/data/repository/rating_repository.dart';
 import 'package:frontend/data/repository/ride_repository.dart';
 import 'package:frontend/data/repository/user_repository.dart';
+import 'package:frontend/ui/page/find_ride/find_ride_view.dart';
+import 'package:frontend/ui/page/find_ride/find_ride_viewmodel.dart';
 import 'package:frontend/ui/page/home/home_view.dart';
 import 'package:frontend/ui/page/home/home_viewmodel.dart';
 import 'package:frontend/ui/page/profile/profile_view.dart';
 import 'package:frontend/ui/page/profile/profile_viewmodel.dart';
 
-// import 'package:frontend/data/repository/activity_repository.dart';
-// import 'package:frontend/data/repository/authentication_repository.dart';
-// import 'package:frontend/data/repository/location_repository.dart';
-// import 'package:frontend/data/repository/reward_repository.dart';
-// import 'package:frontend/data/repository/ride_repository.dart';
-// import 'package:frontend/data/repository/user_repository.dart';
-// import 'package:frontend/ui/page/arrange_pickup/arrange_pickup_viewmodel.dart';
-// import 'package:frontend/data/repository/rating_repository.dart';
-// import 'package:frontend/ui/page/activities/activities_viewmodel.dart';
-// import 'package:frontend/ui/page/create_ride/create_ride_view.dart';
-// import 'package:frontend/ui/page/create_ride/create_ride_viewmodel.dart';
-// import 'package:frontend/ui/page/find_ride/find_ride_view.dart';
-// import 'package:frontend/ui/page/find_ride/find_ride_viewmodel.dart';
-// import 'package:frontend/ui/page/forgot_password/forgot_password_view.dart';
-// import 'package:frontend/ui/page/home/home_viewmodel.dart';
-// import 'package:frontend/ui/page/create_ride/rides_list_viewmodel.dart';
-// import 'package:frontend/ui/page/profile/profile_view.dart';
-// import 'package:frontend/ui/page/profile/profile_viewmodel.dart';
-// import 'package:frontend/ui/page/sign_in/sign_in_view.dart';
-// import 'package:frontend/ui/page/sign_up/sign_up_view.dart';
-// import 'package:frontend/ui/page/home/home_view.dart';
-// import 'package:frontend/ui/page/activities/activities_view.dart';
-// import 'package:frontend/ui/page/create_ride/rides_list_view.dart';
-// import 'package:frontend/ui/page/sign_in/sign_in_viewmodel.dart';
-// import 'package:frontend/ui/page/sign_up/sign_up_viewmodel.dart';
-// import 'package:frontend/ui/page/rating/rating_viewmodel.dart';
-// import 'package:frontend/ui/page/report/report_view.dart';
-// import 'package:frontend/data/repository/report_repository.dart';
-// import 'package:frontend/ui/page/report/report_viewmodel.dart';
-// import 'package:frontend/ui/page/arrange_pickup/arrange_pickup_view.dart';
-// import 'package:frontend/data/model/ride.dart';
-// import 'package:frontend/data/model/driver.dart';
-// import 'package:frontend/data/service/pickup_service.dart';
-// import 'package:frontend/data/repository/pickup_repository.dart';
-
 class App extends StatelessWidget {
-  // final RideRepository _rides = RideRepository();
-  // final PickupRepository _pickupRepository = PickupRepository(
-  //   pickupService: PickupService(),
-  // );
   final UserRepository _userRepository = MockUserRepository();
   final RatingRepository _ratingRepository = MockRatingRepository();
   final RideRepository _rideRepository = MockRideRepository();
   final LocationRepository _locationRepository = MockLocationRepository();
+
+  // final PickupRepository _pickupRepository = PickupRepository(
+  //   pickupService: PickupService(),
+  // );
   // final AuthenticationRepository _authenticationRepository =
   //     AuthenticationRepository();
   // final RewardRepository _rewardRepository = RewardRepository();
   // final ActivityRepository _activityRepository = ActivityRepository();
   // final ReportRepository _reportRepository = ReportRepository();
+
+  late final FindRideViewModel findRideViewModel = FindRideViewModel(
+    rideRepository: _rideRepository,
+  );
+
+  late final HomeViewModel homeViewModel = HomeViewModel(
+    userRepository: _userRepository,
+    locationRepository: _locationRepository,
+  );
+
+  late final ProfileViewModel profileViewModel = ProfileViewModel(
+    userRepository: _userRepository,
+    ratingRepository: _ratingRepository,
+    rideRepository: _rideRepository,
+  );
 
   // late final RidesListViewModel ridesViewModel = RidesListViewModel(
   //   rideRepository: _rides,
@@ -70,15 +52,6 @@ class App extends StatelessWidget {
   //   rideRepository: _rides,
   // );
 
-  // late final FindRideViewModel findRideViewModel = FindRideViewModel(
-  //   rideRepository: _rides,
-  // );
-
-  late final HomeViewModel homeViewModel = HomeViewModel(
-    userRepository: _userRepository,
-    locationRepository: _locationRepository,
-  );
-
   // late final SignInViewModel signInViewModel = SignInViewModel(
   //   _authenticationRepository,
   //   _userRepository,
@@ -87,12 +60,6 @@ class App extends StatelessWidget {
   // late final SignUpViewModel signUpViewModel = SignUpViewModel(
   //   userRepository: _userRepository,
   // );
-
-  late final ProfileViewModel profileViewModel = ProfileViewModel(
-    userRepository: _userRepository,
-    ratingRepository: _ratingRepository,
-    rideRepository: _rideRepository,
-  ); // Load user data on app start
 
   // late final RewardViewModel rewardViewModel = RewardViewModel(
   //   rewardRepository: _rewardRepository,
@@ -127,7 +94,7 @@ class App extends StatelessWidget {
         // '/forgot_password': (context) => ForgotPasswordView(),
         // '/sign_up': (context) => SignUpView(viewModel: signUpViewModel),
         '/home': (context) => HomeView(viewModel: homeViewModel),
-        // '/find_ride': (context) => FindRideView(viewModel: findRideViewModel),
+        '/find_ride': (context) => FindRideView(viewModel: findRideViewModel),
         // '/create_ride':
         //     (context) => CreateRideView(viewModel: createRideViewModel),
         // // '/rewards': (context) => RewardView(viewModel: rewardViewModel),
