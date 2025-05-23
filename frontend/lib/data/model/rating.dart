fe3af7a4602887ffc9d03fa4fb71af9461138fec
Vasilis Mylonas <vasilismylonas@protonmail.com>
2025-05-23 20:17:52 +0300

    Rating Repository and Model

--- frontend/lib/data/model/rating.dart
+++ frontend/lib/data/model/rating.dart
@@ -1,35 +1,8 @@
-class Rating {
-  final int id;
-  final int fromUserId;
-  final int toUserId;
-  final String rideId;
-  final int score;
-  final String? comment;
-  final DateTime createdAt;
+import 'package:frontend/data/model/user.dart';
 
-  Rating({
-    required this.id,
-    required this.fromUserId,
-    required this.toUserId,
-    required this.rideId,
-    required this.score,
-    this.comment,
-    required this.createdAt,
-  });
-
-  factory Rating.random() {
-    final score = (1 + (5 - 1) * (0.5)).toInt();
-    final comment = 'This is a random comment';
-    final createdAt = DateTime.now();
-
-    return Rating(
-      id: 0,
-      fromUserId: 1,
-      toUserId: 2,
-      rideId: 'x',
-      score: score,
-      comment: comment,
-      createdAt: createdAt,
-    );
-  }
+abstract interface class Rating {
+  User get fromUser;
+  User get toUser;
+  String get comment;
+  int get stars;
 }
