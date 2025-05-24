7c05b4eae02182cf21cf33eb40370e8f1f108238
yane <giannis.ravasopoulos@gmail.com>
2025-05-24 18:11:35 +0300

    Add window size plugin support for cross-platform compatibility to simulate android-like app view

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:window_size/window_size.dart';
 import 'package:frontend/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // setWindowTitle('Loop App');
+    setWindowFrame(
+      const Rect.fromLTWH(300, 500, 600, 900),
+    ); // Default size and position
+  }
+
   runApp(App());
 }
